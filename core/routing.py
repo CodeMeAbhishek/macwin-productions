18eb48cb4b411f4d361649862fcc96bee369ee82
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-26 11:22:43 +0530

    web-socket-added

---
+++ core/routing.py
@@ -0,0 +1,17 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from chat.consumers import ChatConsumer
+
+websocket_urlpatterns = [
+    path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
+}) 
