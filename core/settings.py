4b14174729ebb599eac03c3657205ee323fdfb02
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-26 15:48:47 +0530

    chore: switch email backend to Outlook SMTP

--- core/settings.py
+++ core/settings.py
@@ -126,12 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shyinenuv@gmail.com'
-EMAIL_HOST_PASSWORD = 'izjf qawk muqb rymg'
-DEFAULT_FROM_EMAIL = 'shyinenuv@gmail.com'
+EMAIL_HOST_USER = 'shyinenuv@outlook.com'
+EMAIL_HOST_PASSWORD = 'keitcnrlqwvqpxsx'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 60
 EMAIL_USE_SSL = False
 
