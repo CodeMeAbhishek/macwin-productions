00ffc2eebdf1c747966f0a343b2e9dda03aedb5e
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-26 14:59:18 +0530

    django-app-intialized first

--- core/asgi.py
+++ core/asgi.py
@@ -9,14 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django first
+django_asgi_app = get_asgi_application()
+
+# Only import these after Django is initialized
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
