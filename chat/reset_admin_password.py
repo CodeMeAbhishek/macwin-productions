f04817ac29eb6eec7375ca2ce13bc9072592b9fb
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-27 19:28:02 +0530

    new-superuser

---
+++ chat/reset_admin_password.py
@@ -0,0 +1,24 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if not username or not password:
+    print("Please set DJANGO_SUPERUSER_USERNAME and DJANGO_SUPERUSER_PASSWORD environment variables.")
+else:
+    try:
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        print(f"Password for superuser '{username}' has been reset and privileges ensured.")
+    except User.DoesNotExist:
+        print(f"Superuser '{username}' does not exist.") 
