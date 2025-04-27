a7adc198a259e77d969553bd5f1f2345700ba21d
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-27 18:38:39 +0530

    super-user

--- chat/apps.py
+++ chat/apps.py
@@ -1,9 +1,23 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
+import os
 
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Superuser {username} created.")
+    else:
+        print(f"Superuser {username} already exists.")
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
 
-def ready(self):
-    import chat.signals
+    def ready(self):
+        import chat.signals
+        post_migrate.connect(create_superuser, sender=self)
