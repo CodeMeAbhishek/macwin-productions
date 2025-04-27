0cbbaf986cb2761e657de6b44c989bba9f951d68
code-with-abhishek-gupta <gupta.b.abhishek@nuv.ac.in>
2025-04-27 19:35:16 +0530

    resert-pass1

--- chat/reset_admin_password.py
+++ chat/reset_admin_password.py
@@ -1,4 +1,7 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print("PYTHONPATH:", sys.path)
 print("Current working directory:", os.getcwd())
 import django
 
